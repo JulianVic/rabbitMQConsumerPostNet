752c4a762a329c79e44d4283e0a9eef47f4007b0
JulianVic <josejulianvicenteperez@gmail.com>
2024-02-27 19:12:58 -0600

    Consumer listo, sin petición a API2

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+import { connect } from 'amqplib';
+import axios from 'axios';
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+
+const app = express();
+const port = process.env.PORT;
+
+const url : string = process.env.AMQP_URL!;
+const queue : string = process.env.AMQP_QUEUE!;
+const urlApi : string = process.env.API_URL!;
+
+app.listen(port, async () => {
+  console.log(`Server running on port ${port}`);
+
+  const connection = await connect(url);
+  const channel = await connection.createChannel();
+  await channel.assertQueue(queue);
+  channel.consume(queue, async (message) => {
+    if (message) {
+      const data = JSON.parse(message.content.toString());
+      console.log(data);
+      // try {
+      //   const response = await axios.post(urlApi, data);
+      //   console.log(response.data);
+      // } catch (error) {
+      //   console.error(error);
+      // }
+      channel.ack(message);
+    }
+  });
+});
